c651d91c971dd2d40c2e5fda90d7c137f1319142
Shikha Singh <91380174+shikhasingh229@users.noreply.github.com>
2021-11-26 20:26:57 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,25 @@
+var Person1 = 
+{
+    firstName:"Damon",
+    lastName:"Salvatore",
+    officeAddress:"Mystic Falls, Virgina",
+}
+var Person2 =
+{
+    firstName:"Klaus",
+    lastName:"Mikaelson",
+    officeAddress:"New Orleans, Louisiana",
+}
+var Person3 =
+{
+    firstName:"Katherin",
+    lastName:"Pierce",
+    officeAddress:"Bulgaria",
+}
+
+console.log(Person1);
+console.log(Person2);
+console.log(Person3);
+console.log("Full Name Of Person 1 is "+Person1.firstName + " " +Person1.lastName);
+console.log("Full Name Of Person 2 is "+Person2.firstName + " " +Person2.lastName);
+console.log("Full Name Of Person 3 is "+Person3.firstName + " " +Person3.lastName);
