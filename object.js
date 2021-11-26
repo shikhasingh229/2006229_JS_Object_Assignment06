8e5d0ad68208cb47aec8d129b135999d6cdd3fa1
Shikha Singh <91380174+shikhasingh229@users.noreply.github.com>
2021-11-26 20:49:12 +0530

    Update object.js

--- object.js
+++ object.js
@@ -23,3 +23,17 @@ console.log(Person3);
 console.log("Full Name Of Person 1 is "+Person1.firstName + " " +Person1.lastName);
 console.log("Full Name Of Person 2 is "+Person2.firstName + " " +Person2.lastName);
 console.log("Full Name Of Person 3 is "+Person3.firstName + " " +Person3.lastName);
+function print(person)
+{
+    console.log(person);
+    console.log("First name is "+person.firstName);
+    console.log("Last name is "+person.lastName);
+    console.log("Office Address is "+person.officeAddress);
+}
+
+console.log(Person1);
+console.log(Person2);
+console.log(Person3);
+print(Person1);
+print(Person2);
+print(Person3);
